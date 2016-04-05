5e0c7ebbb991723dd87fb644b3611b1645fe32d1
Eric Gonzalez <eric@plasticgnome.com>
2016-04-04 23:16:17 -0700

    Video Storage Class 	- Prevent re-downloading videos

--- tv/src/main/java/com/fourty_eight_dps/marclay/media/MediaDispatcher.java
+++ tv/src/main/java/com/fourty_eight_dps/marclay/media/MediaDispatcher.java
@@ -5,20 +5,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
-import android.preference.PreferenceManager;
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.fourty_eight_dps.marclay.core.firebase.FirebaseRefs;
 import com.fourty_eight_dps.marclay.core.firebase.Video;
-import com.google.common.collect.Iterators;
 import java.io.File;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -28,8 +24,7 @@ public class MediaDispatcher implements ChildEventListener {
 
   private Firebase videos;
   private DownloadManager downloadManager;
-  private SharedPreferences sharedPreferences;
-  private Iterator<?> cycleIterator;
+  private VideoStorage videoStorage;
   private Context context;
 
   /**
@@ -39,9 +34,8 @@ public class MediaDispatcher implements ChildEventListener {
 
   public MediaDispatcher(Context context) {
     this.context = context;
-    this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    updateCycleIterator();
-    downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+    this.downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+    this.videoStorage = new VideoStorage(context);
 
     IntentFilter downloadCompleteFilter =
         new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
@@ -49,7 +43,7 @@ public class MediaDispatcher implements ChildEventListener {
       @Override public void onReceive(Context context, Intent intent) {
         long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
         if (keyToDownloadIdMap.containsKey(downloadId)) {
-          keyToDownloadIdMap.remove(downloadId);
+          String videoId = keyToDownloadIdMap.remove(downloadId);
           DownloadManager.Query query = new DownloadManager.Query();
           query.setFilterById(downloadId);
           Cursor cursor = downloadManager.query(query);
@@ -62,18 +56,11 @@ public class MediaDispatcher implements ChildEventListener {
             int fileNameIndex = cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME);
             String uriLocation = cursor.getString(uriIndex);
             String fileName = cursor.getString(fileNameIndex);
-            sharedPreferences.edit()
-                .putString(uriLocation, fileName)
-                .apply();
-            updateCycleIterator();
+            videoStorage.putVideo(videoId, fileName);
           }
         }
       }
     }, downloadCompleteFilter);
-  }
-
-  private void updateCycleIterator() {
-    this.cycleIterator = Iterators.cycle(sharedPreferences.getAll().values());
   }
 
   public void onCreate() {
@@ -82,10 +69,7 @@ public class MediaDispatcher implements ChildEventListener {
   }
 
   public File nextVideo() {
-    if (!cycleIterator.hasNext()) {
-      return null;
-    }
-    return new File(cycleIterator.next().toString());
+    return videoStorage.next();
   }
 
   @Override public void onChildAdded(DataSnapshot dataSnapshot, String s) {
@@ -93,27 +77,22 @@ public class MediaDispatcher implements ChildEventListener {
     Video video = dataSnapshot.getValue(Video.class);
     String key = dataSnapshot.getKey();
 
-    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(video.getUrl()));
-    request.setVisibleInDownloadsUi(true);
-    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
-    request.setDestinationInExternalFilesDir(context, null, dataSnapshot.getKey());
-    keyToDownloadIdMap.put(downloadManager.enqueue(request), key);
+    if (!videoStorage.hasVideo(key)) {
+      DownloadManager.Request request = new DownloadManager.Request(Uri.parse(video.getUrl()));
+      request.setVisibleInDownloadsUi(true);
+      request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+      request.setDestinationInExternalFilesDir(context, null, dataSnapshot.getKey());
+      keyToDownloadIdMap.put(downloadManager.enqueue(request), key);
+    }
   }
 
-  @Override public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-  }
+  @Override public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
 
   @Override public void onChildRemoved(DataSnapshot dataSnapshot) {
     // TODO Deleted the file
-
   }
 
-  @Override public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+  @Override public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
 
-  }
-
-  @Override public void onCancelled(FirebaseError firebaseError) {
-
-  }
+  @Override public void onCancelled(FirebaseError firebaseError) {}
 }
---
+++ tv/src/main/java/com/fourty_eight_dps/marclay/media/VideoStorage.java
@@ -0,0 +1,43 @@
+package com.fourty_eight_dps.marclay.media;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import com.google.common.collect.Iterators;
+import java.io.File;
+import java.util.Iterator;
+
+/**
+ * A wrapper around shared preferences for persisting video locations
+ */
+public class VideoStorage {
+
+  private SharedPreferences sharedPreferences;
+  private Iterator<?> cycleIterator;
+
+  public VideoStorage(Context context) {
+    this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+  }
+
+  public boolean hasVideo(String videoId) {
+    return sharedPreferences.contains(videoId);
+  }
+
+  public void putVideo(String videoId, String fileUri) {
+    sharedPreferences.edit()
+        .putString(videoId, fileUri)
+        .apply();
+    updateCycleIterator();
+  }
+
+  public File next() {
+    if (!cycleIterator.hasNext()) {
+      return null;
+    }
+    return new File(cycleIterator.next().toString());
+  }
+
+  private void updateCycleIterator() {
+    this.cycleIterator = Iterators.cycle(sharedPreferences.getAll().values());
+  }
+}
