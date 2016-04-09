fb1ed33fadade00ffe7184302128aa107f91650f
Eric Gonzalez <eric@plasticgnome.com>
2016-04-08 22:14:03 -0700

    Added Location to Video model

--- core/src/main/java/com/fourty_eight_dps/marclay/core/firebase/Video.java
+++ core/src/main/java/com/fourty_eight_dps/marclay/core/firebase/Video.java
@@ -3,8 +3,13 @@ package com.fourty_eight_dps.marclay.core.firebase;
 public class Video {
 
   String url;
+  String location;
 
   public String getUrl() {
     return url;
+  }
+
+  public String getLocation() {
+    return location;
   }
 }
