1b88ea286ce53c503d35e794f32e7a1541d9dc86
Eric Gonzalez <eric@plasticgnome.com>
2016-04-09 16:08:43 -0700

    Set Switch Based on Notification State

--- mobile/src/main/java/com/fourty_eight_dps/marclay/MainActivity.java
+++ mobile/src/main/java/com/fourty_eight_dps/marclay/MainActivity.java
@@ -5,16 +5,33 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Switch;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+  @Bind(R.id.switch_notification)
+  Switch notificationSwitch;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    if (hasNotificationsEnabled()) {
+      notificationSwitch.setChecked(true);
+    }
+  }
+
+  private boolean hasNotificationsEnabled() {
+    return Settings.Secure.getString(getContentResolver(), "enabled_notification_listeners")
+        .contains(getApplicationContext().getPackageName());
   }
 
   @OnClick(R.id.switch_notification)
